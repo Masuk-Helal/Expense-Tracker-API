a223964092651f49bd7ef0e1695f2031b2d92ba2
TashdidDiganta <tashdiddiganta@gmail.com>
2026-08-23 22:04:08 +0600

    database create

--- database.py
+++ database.py
@@ -3,13 +3,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/ExpanceTrakerDatabase'
 
-import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not SQLALCHEMY_DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not configured")
+
+connect_args = {}
+
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
 
 connect_args = {'check_same_thread': False} if SQLALCHEMY_DATABASE_URL.startswith('sqlite') else {}
 
