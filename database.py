be8a723ceeee4637a2c2fba4a74b565d14693339
TashdidDiganta <tashdiddiganta@gmail.com>
2026-08-24 12:40:56 +0600

    supabase added

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@127.0.0.1:3306/ExpanceTrakerDatabase'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.ypvzfxqvibubthdzeolw:5700asdf@asdf@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 
 # SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
